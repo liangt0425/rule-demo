f1f2d1be618d2d4e98bbec9764bfc9813ae0ff65
tong104218 <40078764+tong104218@users.noreply.github.com>
2022-11-14 19:40:56 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <SketchRule />
+</template>
+
+<script>
+import { SketchRule } from 'vue3-sketch-ruler'
+import 'vue3-sketch-ruler/lib/style.css'
+
+export default {
+  name: 'App',
+  components: {
+    SketchRule
+  }
+}
+</script>
+
